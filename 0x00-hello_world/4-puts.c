053390b3f54814f4b4f527af2323416aac45fe1d
mrblackduke <blackdukeibifubara@gmail.com>
2023-09-10 00:58:37 -0700

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - my first c program prints a line using puts i'm not allowed to use printf
+ * main - my first c program prints a line using puts no printf
  * Return: 0 for pass
  */
 int main(void)
