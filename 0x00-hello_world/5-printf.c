c8b620e9cd4ca80cfda3113ee59da8c9a161c5b2
mrblackduke <blackdukeibifubara@gmail.com>
2023-09-10 01:35:41 -0700

    5 printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
  * main - print using printf
- * Return: 0 pass
+ * Return: 0 (pass)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
