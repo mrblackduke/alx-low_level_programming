c7b3cf5a81dbc041a5ae3ba54eb65afcb0071515
mrblackduke <blackdukeibifubara@gmail.com>
2023-09-10 00:56:28 -0700

    4puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - my first c program prints a line using puts i'm not allowed to use printf
+ * Return: 0 for pass
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+
