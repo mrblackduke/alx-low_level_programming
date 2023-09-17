919009d468f1f52a5eee50e5f9476493020b8dd5
mrblackduke <blackdukeibifubara@gmail.com>
2023-09-17 09:14:35 -0700

    numbers 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print numbers
+ * Return: 0 (pass)
+ */
+int main(void)
+{
+	int dec;
+	for (dec = '1'; dec <= '9'; ++dec;)
+	printf("numbers = %d '\n'",dec);
+	return (0);
+}
