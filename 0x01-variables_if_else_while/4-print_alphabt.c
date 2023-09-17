7cbe511fd699f5cb8fd1b06b4b8979f061485f46
mrblackduke <blackdukeibifubara@gmail.com>
2023-09-17 08:59:00 -0700

    print without q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,10 +7,9 @@ int main(void)
 {
 	char alpha;
 
-	for (alpha = 'a'; alpha <= 'z';)
+	for (alpha = 'a'; alpha <= 'z'; ++alpha)
 		if (alpha != 'e' && alpha != 'q')
 		putchar(alpha);
-	++alpha;
 	putchar('\n');
 	return (0);
 }
