d6749d5e3b288fb2cee27123fed36574a3e4c9f1
mrblackduke <blackdukeibifubara@gmail.com>
2023-09-17 06:53:54 -0700

    lowercase alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,7 +7,8 @@ int main(void)
 {
 	char alpha;
 
-	for (alpha = 'a'; alpha <= 'z'; ++alpha;)
-	putchar(alpha, '\n');
+	for (alpha = 'a'; alpha <= 'z'; ++alpha; )
+	putchar(alpha);
+	putchar('\n');
 	return (0);
 }
