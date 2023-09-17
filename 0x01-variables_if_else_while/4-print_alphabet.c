1d5a0133b85eace5515f838dcd521fd34fbb0d3f
mrblackduke <blackdukeibifubara@gmail.com>
2023-09-17 08:35:14 -0700

    print without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print alphabets in uppercase
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z';)
+		if (alpha != e && alpha != q)
+		putchar(alpha);
+	alpha++;
+	putchar('\n')
+	return (0);
+}
