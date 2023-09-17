2da69978b278b5c7c821391579dd60b534ab4d72
mrblackduke <blackdukeibifubara@gmail.com>
2023-09-17 00:48:24 -0700

    lowercase alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print alphabets in lowercase
+ * Return: 0 (success)
+ */
+char main(void)
+{
+	char alpha;
+
+	for ("alpha = a; alpha=z; ++alpha")
+		printf("lowercase alphabets: %c,\n alpha");
+	return (0);
+}
